40509756faae48d217e33b2e7e67ffc5740750fb
Simon Rodriguez <kosua20@gmail.com>
2025-08-31 15:27:42 +0200

    naadirossem.net: basic login for blog (but content was lost).

---
+++ www.naadirossem.net/blog/index.php
@@ -0,0 +1,19 @@
+﻿<?php 
+
+if(!isset($_POST['login']) || !isset($_POST['pwd']) || strcasecmp($_POST["login"], "jessica") != 0 || strcasecmp($_POST["pwd"], "remember") != 0){
+	echo "<script language=\"javascript\">window.top.location.href=\"../index.html\"</script>";
+	// Early 'exit'
+	return;
+}
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title></title>
+</head>
+<body bgcolor="000000">
+<p style="text-align: center; font-size: 40px; color: white; margin-top: 20%;">?</p>
+</body>
+</html>
